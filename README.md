443a6135e97b2bfd113528e4c0203872c475c643
spatel13 <sahil.patel2461@gmail.com>
2021-07-04 16:36:16 -0400

    Added pandas to simplify data cleaning process

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This project is written to help decide if I should buy a stock based on whether they have gone down in value over the last 3 days
 
 ## Future TO-DO
-* Implement a Dockerfile and scripts to run docker
 * Implement an API in Python
+* Implement a Dockerfile and scripts to run docker
 * Add React Frontend to the Python API
 * Add text message to API
--- main.py
+++ main.py
@@ -1,44 +1,28 @@
-import json
 import os
 import requests
+import pandas as pd
 
 
 API_KEY=os.environ['ALPHAVANTAGE_API_KEY']
 
 def get_recent_5_datapoints(stock):
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol={stock.upper()}&apikey={API_KEY}"
-    stock_data = get_data(url, 5)
-    return stock_data
-
-def get_data(url, days=-1):
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol={stock.upper()}&apikey={API_KEY}&datatype=csv"
     response = requests.get(url)
-    json_stock_data = response.json()
+    if "Error Message" not in response.content.decode('utf-8'):
+        outerData = response.content.decode('utf-8').split('\n')
+        for i in range(len(outerData)):
+            outerData[i] = outerData[i].strip().split(",")
 
-    if "Error Message" not in list(json_stock_data.keys()):
-        stock_data = json_stock_data.get(list(json_stock_data.keys())[1])
-        recent_5_datapoints = list(stock_data.keys())[:days]
+        # print(outerData)
+        data = pd.DataFrame(outerData[1:], columns=outerData[0])
+        return list(data.head().close)
 
-        rtn_data = {}
-        for datapoint in recent_5_datapoints:
-            daily = {}
-            for section in list(stock_data.get(datapoint).keys()):
-                section_name = section.split(" ")
-                daily[section_name[1]] = float(stock_data.get(datapoint).get(section))
-            rtn_data[datapoint] = daily
-
-        return rtn_data
     return None
 
 def check_3_days_rule(data):
-    three_day_close = []
-    for days in list(data.keys())[:4]:
-        for key in list(data.get(days).keys()):
-            if key == 'close':
-                three_day_close.append(data.get(days).get(key))
-
     difference_in_days = []
-    for i in range(len(three_day_close) - 1):
-        difference_in_days.append(float(three_day_close[i]) - float(three_day_close[i+1]))
+    for i in range(len(data) - 1):
+        difference_in_days.append(float(data[i]) - float(data[i+1]))
 
     time_to_buy = True
     for price_diff in difference_in_days:
